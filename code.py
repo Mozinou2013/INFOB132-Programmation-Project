d7469cee9d88fde5518099ba5906cef97ab18e82
ldelpier <92252569+ldelpier@users.noreply.github.com>
2022-02-24 11:57:12 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,32 @@
+
+def attack(player_A, player_B, order): 
+    '''
+    Attacks the wolves of the other player 
+    Parameters
+    ----------
+    player_A : wolves of the player who attacks (dict)
+    player_B : wolves of the player who is attacked (dict)
+    orders : orders of the player who attacks (str)
+    Returns
+    --------
+    player_1: wolves of the player 1 (dict)
+    player_2: wolves of the player 2 (dict)
+    Versions
+    --------
+    specification : Eline Mota (v.1 17/02/2022) 
+    '''
+    # varible de test/ modifiable 
+    order = ordertuple  # prensdre la fonction donnant les ordres
+    player_A [(x,y)[life]] = (player_A [(x,y)[life]] - ) - attack # énergie loup au debut du cours 
+    if '*' in ordertuple: # lire l'ordre et voir si * si trouve 
+        # voir ce qui se passe sur blessed 
+        player_A[(x,y)] = int(player_B [(x,y)[life]] / 0.1) # quand un loup attaque, perte 1/10 de son énergie 
+
+    if player_A [(x,y)[life]] == 0  or player_B [(x,y)[life]] == 0
+        player_A [(x,y)] == 'humain'
+        player_B [(x,y )]== 'humain' # voir sur blessed 
+
+
+
+
+    
