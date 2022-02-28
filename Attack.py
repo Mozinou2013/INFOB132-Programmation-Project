17f717e791549fde8c38dc3fff3cb1c72905b3d0
AlineBlg <95127586+Mozinou2013@users.noreply.github.com>
2022-02-28 15:11:45 +0100

    Update Attack.py

--- Attack.py
+++ Attack.py
@@ -1,4 +1,3 @@
-
 def attack(player_A, player_B, orders): 
     '''
     Attacks the wolves of the other player 
@@ -13,20 +12,50 @@ def attack(player_A, player_B, orders):
     player_2: wolves of the player 2 (dict)
     Versions
     --------
-    specification : Eline Mota (v.1 17/02/2022) 
-    implementation : Louise Delpierre (v.1 24/02/2022)
+    specification : Eline Mota (v.1 17/02/2022)
+    implementation: Louise Delpierre, Aline Boulanger (v.1 24/02/2022)
     '''
-    # varible de test/ modifiable 
-    order = turn_list(orders)  # prensdre la fonction donnant les ordres
-    player_A [(x,y)["life"]] = (player_A [(x,y)["life"]] - #bonus ) - attack # énergie loup au debut du cours 
-    if '*' in order: # lire l'ordre et voir si * si trouve 
-        # voir ce qui se passe sur blessed 
-        player_A[(x,y)] = int(player_B [(x,y)["life"]] / 0.1) # quand un loup attaque, perte 1/10 de son énergie 
-
-    if player_A [(x,y)["life"]] == 0  or player_B [(x,y)["life"]] == 0
-        player_A [(x,y)] == 'humain'
-        player_B [(x,y)]== 'humain' # voir sur blessed 
-
-
 
     
+    #bonus = #fct bonus 
+    # varible de test/ modifiable 
+    order = turn_list(orders)  # prendre la fonction donnant les ordres
+
+    print('order', order)
+    for elements in order:
+        
+        if '*' in elements: # lire l'ordre et voir si * si trouve 
+            # voir ce qui se passe sur blessed POUR LA VIE 
+            coords = elements.split (":*")
+            
+            
+            i = 0
+            for coord in coords:
+                i += 1
+
+                coord = coord.split ("-")
+                if i == 1:
+                    x_A = int(coord[0])
+                    y_A = int(coord[1])
+                elif i == 2 :
+                    x_B = int(coord[0])
+                    y_B = int(coord[1])
+                
+
+            if player_A [(x_A,y_A)]["life"] == 0 :
+                player_A [(x_A,y_A)] == 'humain' # voir sur blessed
+
+            else :
+                vie = player_A[(x_A,y_A)]["life"] # quand un loup attaque, perte 1/10 de son énergie
+                vie = vie/10
+                print(vie)
+                player_B [(x_B,y_B)]["life"] -= vie
+                
+
+            if player_B [(x_B,y_B)]["life"] == 0 :
+                player_B [(x_B,y_B)] == 'humain' # voir sur blessed
+        #player_A [(x_A, y_A)["life"]] = (player_A[(x_A,y_A)["life"]]) - attack # énergie loup au debut du cours ATTENTION - bonus
+         
+    return player_A, player_B
+
+print(attack (player_1,player_2, "2-2:*7-4 10-10:pacify"))
