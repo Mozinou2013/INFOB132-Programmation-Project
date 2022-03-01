d7a9ff934d7d100372d10a354f22d25cc52b47e2
elineee <99394485+elineee@users.noreply.github.com>
2022-03-01 20:30:44 +0100

    Update engine_gr_07.py
    j'ai rajouté deux nouvelles fonctions qui serviront beaucoup pour la suite et j'ai aussi mis l'implémentation de la fonction create_map

--- engine_gr_07.py
+++ engine_gr_07.py
@@ -38,22 +38,87 @@ def read_file(file):
 
     """
 
-def create_map(lenght, height, player_1, player_2, food): 
+def create_map(width, height, player_1, player_2, food): 
     """ 
     Create the map of the board 
-
     Parameters
     ----------
     height: height of the map (int)
-    lenght: lenght of the map (int)
+    width: lenght of the map (int)
     player_1: wolves of the player 1 (dict)
     player_2: wolves of the player 2 (dict)
     food : food on the map (dict)
-
     Versions
     --------
     specification : Louise Delpierre (v1 17/02/2022)
+    implementation: Eline Mota (v.1 28/02/2022)
     """
+    print(term.home + term.clear)
+    for x in range(0, width * 3, 3):
+        for y in range(0, height, 1):
+            if (x+y)%2 ==0:
+                print(term.move_xy(x, y) + term.on_lavenderblush3 + '   ')
+            else:
+                print(term.move_xy(x, y) + term.on_snow2 + '   ')
+    for key in player_1:
+        if player_1[key]['type'] =='alpha':
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x, y)
+            print(term.move_xy(x, y) + term.red + term.on_limegreen + 'A')
+        elif player_1[key]['type'] =='omega':
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x, y) + 'O')
+        else:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x, y) + 'L')
+    for key in player_2:
+        if player_2[key]['type'] =='alpha':
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x, y)
+            print(term.move_xy(x, y) + term.black +'A')
+        elif player_2[key]['type'] =='omega':
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x, y) + term.black + 'O')
+        else:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x, y) +'L')
+    for key in food:
+        if food[key] == 10:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x,y) +'b')
+        elif food[key] == 30:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x,y) + 'a')
+        elif food[key] == 50:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x,y) + 'm')
+        elif food[key] == 100:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x,y) + 'r')
+        else:
+            x = key[0]
+            y = key[1]
+            x, y = trans_coord(x,y)
+            print(term.move_xy(x,y) + 'd')
+
 
 def attack(player_A, player_B, orders): 
     """ 
@@ -261,3 +326,39 @@ def turn_list(order):
     --------
     specification: Aurélie Genot (v.1 19/02/2022)
     """
+def trans_coord(x, y):
+    """Adapt and return the coordonates of an element for the map
+    Parameters:
+    -----------
+    x: coordonate x (int)
+    y: coordonate y (int)
+    
+    Return:
+    -------
+    x: coordonate x (int)
+    y: coordonate y (int)
+    Version:
+    --------
+    specification: Eline Mota (v.1 1/03/2022)
+    implementation: Eline Mota (v.1 1/03/2022) """
+
+    x = (x-1)*3 + 1
+    y = y-1
+    return x, y
+def clear_pos(x,y):
+    """Clear a position after moving a wolf
+    Parameters:
+    -----------
+    x: coordonate x of the position
+    y: coordonate y of the position
+    
+    Versions:
+    ---------
+    specification: Eline Mota (v.1 01/03/2022)
+    implementation: Eline Mota (v.1 01/03/2022)
+    """
+    if (x+y) % 2 ==0:
+        print(term.move_xy(x, y) + term.on_snow2 + ' ')
+    else:
+        print(term.move_xy(x,y) + term.on_lavenderblush3 + ' ')
+    
