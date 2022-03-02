df30e2867e934d7a25b2c2a095b7723f6e1c4579
AlineBlg <95127586+Mozinou2013@users.noreply.github.com>
2022-03-02 09:16:41 +0100

    Update check_life.py

--- check_life.py
+++ check_life.py
@@ -6,7 +6,7 @@ def check_life(player_A, case):
     case: place of the wolf we want to check (tuple)
     Return:
     -------
-    Result: True if the wolf has zero life, False otherwise (bool)
+    player_A : wolves of the player playing (dict)
     Versions:
     ---------
     specification: Eline Mota (v.1 19/02/2022)
