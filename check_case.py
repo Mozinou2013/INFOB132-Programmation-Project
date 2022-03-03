73c0dd1bc3999dc6696ded2961b2b2f2432c4b01
AurelieGnt <genot.aurelie.7@gmail.com>
2022-03-03 15:38:28 +0100

    Changement check_case et in_map

--- check_case.py
+++ check_case.py
@@ -1,20 +1,24 @@
-def check_case(case, player_A):
-    """Check if there is a wolf on a case
+def check_case(case, dictionnary_checked):
+    """Check if there is an element of the dictionnary on a case 
     
     Parameters:
     -----------
     case: a case of the map (tuple)
-    player_A: wolves of a player (dict)
+    dictionnary_checked: dictionnary in which we check the information(dict)
 
     Return:
     -------
-    Result: True if there is a wolf on the case, False otherwise (bool)
+    Result: True if there is a item on the case, False otherwise (bool)
 
     Versions:
     ---------
     specification: Louise Delpierre (v.1 19/02/2022)
+    specification: Aurélie Genot (v.2 03/03/2022)
     implementation: Aurélie Genot (v.1 03/03/2022)
     """
-
-    ## Regarder si le tuple est déjà occupé par une clé du dico player_A
- 
+    for key in dictionnary_checked :
+        if case == key :
+            return True 
+        else:
+            return False
+                   
