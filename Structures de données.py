bf635e122c949addd06499aff0c921f817fff5bd
AlineBlg <95127586+Mozinou2013@users.noreply.github.com>
2022-03-03 12:44:08 +0100

    Delete Structures de données.py

--- Structures de données.py
+++
@@ -1,5 +0,0 @@
-nourriture = [[(position_x, position_y), vie], [(position_x, position_y), vie]]
-
-player_1 = {(position_x, position_y): {'type': 'A', 'life': number}, (position_x, position_y): {'type': 'B', 'life': number}}
-
-player_2 = {(position_x, position_y): {'type': 'A', 'life': number}, (position_x, position_y): {'type': 'B', 'life': number}}
