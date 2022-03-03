83e578d0cfed20e755536cfbcbf174fb2cd77b71
AlineBlg <95127586+Mozinou2013@users.noreply.github.com>
2022-03-03 10:42:46 +0100

    Update eat.py

--- eat.py
+++ eat.py
@@ -1,3 +1,7 @@
+orders = "2-2:<2-3 10-10:pacify"
+food = {(4,4) : 10, (1, 1) : 50}
+player_1 = {(2,2): {'type': 'alpha', 'life': 90}, (1,1): {'type': 'omega', 'life': 100}}
+
 def eat(food, player_A, orders):
     """
     Add energy point to a wolf depending on what he eats
