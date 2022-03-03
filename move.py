5fd7662941a65a304fb8d2b8c963355f4625fd17
AurelieGnt <genot.aurelie.7@gmail.com>
2022-03-03 10:53:01 +0100

    Create move.py

---
+++ move.py
@@ -0,0 +1,20 @@
+def move(player_A, order):
+    """
+    Move a wolf on the board
+
+    Parameters:
+    -----------
+    player_A: wolves of the player who is playing (dict)
+    order : order of the player_A (str)
+
+    Returns:
+    ---------
+    player_A: wolves of the player who is playing after moving (dict)
+
+    Versions:
+    --------
+    specification: Eline Mota (v.1 19/02/2022)
+
+    """
+
+## 1) Vérifier que le déplacement est possible (sur le plateau + )
