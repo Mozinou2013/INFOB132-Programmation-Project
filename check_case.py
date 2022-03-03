76076aad51403f0e3ba37a48d4a0641fdd96ee68
AurelieGnt <genot.aurelie.7@gmail.com>
2022-03-03 15:09:15 +0100

    Create check_case.py

---
+++ check_case.py
@@ -0,0 +1,20 @@
+def check_case(case, player_A):
+    """Check if there is a wolf on a case
+    
+    Parameters:
+    -----------
+    case: a case of the map (tuple)
+    player_A: wolves of a player (dict)
+
+    Return:
+    -------
+    Result: True if there is a wolf on the case, False otherwise (bool)
+
+    Versions:
+    ---------
+    specification: Louise Delpierre (v.1 19/02/2022)
+    implementation: Aurélie Genot (v.1 03/03/2022)
+    """
+
+    ## Regarder si le tuple est déjà occupé par une clé du dico player_A
+ 
