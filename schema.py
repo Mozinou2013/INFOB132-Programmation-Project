1cde06ad9155ff7fde21610bbaa2c63d6988d4a6
AlineBlg <95127586+Mozinou2013@users.noreply.github.com>
2022-04-20 08:34:28 +0200

    Update schema.py

--- schema.py
+++ schema.py
@@ -3,22 +3,22 @@ def IA_final(player_1, player_2, food, width, height):
     'attribue un numéro à chaque loup pour les distinguer (à voir)'
 
     if 'loup à moins de 2 cases de alpha:':
-        'alpha recule et deux ou trois (ou plus) loups normaux les plus proches viendront en aide'
+        'alpha recule et deux ou trois (ou plus) loups normaux les plus proches viendront en aide' #certains
 
         if 'loup normaux moins de une case et au moins 20 pv':
             'les deux ou trois (ou plus) loups normaux attaquent'
         else:
-            if 'if loups normaux ont plus de 20 pv':
+            if 'if loups normaux ont plus de 20 pv': #20 un peu peu ? 
                 'loups normaux continuent à se rapprocher'
             else:
                 if 'loups normaux près de nourriture':
                     'mange la nourriture'
                 else:
-                    'se rapproche de la nourriture (si possible dans le sens de l alpha'
+                    'se rapproche de la nourriture (si possible dans le sens de l alpha'  #laisser une proche de l'alpha pour lui
 
         if 'omega a plus de 40 de vie et plus d un loup dans un rayon de 3-4 cases':
             'omega pacifie'
-        elif 'omega a moins de 40 de vie ou un seul loup dans un rayon de 3-4 cases': #peut-être mettre "et" à la place de "ou"
+        elif 'omega a moins de 40 de vie ou un seul loup dans un rayon de 3-4 cases': 
             'omega se recule avec alpha'
         
         if 'autres loups normaux pas venus en défense à moins de une case de alpha ou omega adverses':
@@ -27,17 +27,17 @@ def IA_final(player_1, player_2, food, width, height):
             if 'omega adverse plus de 40 pv':
                 'loups normaux se deplacent vers omega'
             else: 'omega adverse moins de 40':
-                'se déplacent vers alpha' #attaque oméga adverse et si oméga mort, on attaque alpha 
+                'se déplacent vers alpha' 
         
     else: 'loups adverses à plus de deux cases de alpha et omega':
 
-        if 'alpha ou omega ne sont pas à 100':
+        if 'alpha ou omega ne sont pas à 100': #100 un peu bcp ? 80 ?
             'manger ou se déplacer ensemble vers la source de nourriture la plus proche'
 
         else:
 
             if 'loup adverses à moins de 5 cases de distances':
-                'reculent ensemble' #et pacifie ? 
+                'reculent ensemble' 
 
             else:
 
@@ -48,7 +48,7 @@ def IA_final(player_1, player_2, food, width, height):
                      'ne bougent pas'
         
         if 'autres loups normaux à moins de 20 pv':
-            if 'nourriture à une case de distance': #à maximum une case 
+            if 'nourriture à une case de distance': 
                 'mangent'
             else:
                 'se déplacent vers nourriture la plus proche' 
@@ -59,17 +59,17 @@ def IA_final(player_1, player_2, food, width, height):
                 if 'omega a plus de 39 pv':
                     if 'loups normaux a 100 pv':
                         'se rapprochent de omega'
-                    elif 'loups normaux moins de 100':
+                    elif 'loups normaux moins de 100': #100 un peu bcp ?
                         if 'nourriture à une case de distance':
                             'mangent'
                         else:
                             'se rapprochent de omega'
 
-                else: '(omega moins de 39 pv)':
+                else: '(omega moins de 39 pv)': #40 ?
                     if 'loups normaux a 100 pv': #peut-être mettre de 80 à 100 pv parce que je ne pense pas qu'ils seront souvent à 100 tout pile ?
                         'se rapprochent de alpha'
                     elif 'loups normaux moins de 100':
                         if 'nourriture à une case de distance':
                             'mangent'
                         else:
-                            'se rapprochent de alpha' #du coup si oméga adverse a moins de 39 pv, d'après le rapport, on l'attaque lui avant d'aller attaquer l'alpha
+                            'se rapprochent de alpha' 
