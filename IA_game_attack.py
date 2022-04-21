bb9ec95bf14ddf0609cc8ee6673001428363afea
AurelieGnt <genot.aurelie.7@gmail.com>
2022-04-21 11:14:03 +0200

    Attack - eat

--- IA_game_attack.py
+++ IA_game_attack.py
@@ -64,30 +64,3 @@ def IA_game_attack(player_A, player_B, orders):
                 
     return orders
 
-
-def IA_game_move(player_A, player_B, food, orders):
-    """
-    To make the wolves move
-
-    Parameters
-    ----------
-    player_A : wolves of the IA (dict)
-    player_B : wolves of the other player (dict)
-    food: food of the game (dict)
-    orders: orders of the IA (str)
-
-    Returns
-    -------
-    orders: orders of the IA (str)
-
-    Versions:
-    ---------
-    specification: Aurélie Genot et Aline Boulanger (v.1 31/03/2022)
-    implementation : Aurélie Genot et Aline Boulanger (v.1 31/03/2022)
-    """
-    ##ATTENTION SI LOUP ADVERSAIRE PEUT PAS BOUGER 
-    for wolves in player_A:
-        for wolves in player_B: 
-
-            #si vie omega >30: on move sinon on va attack mais faut être a cote sinon faut move 
-            #Si omega dead -> move vers alpha pr attack alpha 
--- IA_game_eat.py
+++ IA_game_eat.py
@@ -1,12 +1,32 @@
+pos_alpha_omega = {pos_alpha : 'alpha', pos_omega : 'omega'}
 
-def IA_game_eat(player_A, food, orders):
+## PARTIE OMEGA ET ALPHA MANGENT 
+def IA_food_eaten(wolves, food)
+
+
+## PARTIE LES LOUPS MANGENT 
+for wolves in player_A:
+        for foods in food:
+    
+            distance = count_cases(wolves, foods)
+            distance_x = distance[0]
+            distance_y = distance[1]
+            food_eaten = foods[key]
+            if distance_x <= 1 and distance_y <=1: 
+                #vérifie que l'action de se nourrir ait un minimum de sens
+                if 100 > player_A[wolves]['life'] and food[foods]['life'] > 0 :
+                    IA_game_eat(wolf, food_eaten, orders)  
+
+
+## POUR CREER L'ORDRE FOOD 
+def IA_game_eat(wolf, food_eaten, orders):
     """
     To make the wolves eat
 
     Parameters
     ----------
-    player_A : wolves of the IA (dict)
-    food: food of the game (dict)
+    wolf: wolf which will eat (tuple)
+    food_eaten: food eaten by the wolf (tuple)
     orders: orders of the IA (str)
 
     Returns
@@ -16,23 +36,17 @@ def IA_game_eat(player_A, food, orders):
     Versions:
     ---------
     specification: Aurélie Genot et Aline Boulanger (v.1 31/03/2022)
+    specification : Aurélie Genot et Aline Boulanger (v.2 20/04/2022)
     implementation : Aurélie Genot et Aline Boulanger (v.1 31/03/2022)
+    implementation : Aurélie Genot et Aline Boulanger (v.2 21/04/2022)
     """
-    for wolves in player_A:
-        for foods in food:
-    
-            distance = count_cases(wolves, foods)
-            distance_x = distance[0]
-            distance_y = distance[1]
-            if distance_x <= 1 and distance_y <=1: 
-                #vérifie une série de chose pour que l'action de se nourrir ait un minimum de sens
-                if 100 > player_A[wolves]['life'] and food[foods]['life'] > 0 :  
-                    eat_x = str(wolves[0])
-                    eat_y = str(wolves[1])
-                    eaten_x = str(foods[0])
-                    eaten_y = str(foods[1])
-                    action_2 = ':<'
-                    if can_use(orders, int(eat_x), int(eat_y)) == True: 
-                        orders += orders + eat_x + '-' + eat_y + action_2 + eaten_x + '-' + eaten_y + ' '
+    eat_x = str(wolf[0])
+    eat_y = str(wolf[1])
+    eaten_x = str(food_eaten[0])
+    eaten_y = str(food_eaten[1])
+
+    action = ':<'
+    if can_use(orders, int(eat_x), int(eat_y)) == True: 
+        orders += orders + eat_x + '-' + eat_y + action + eaten_x + '-' + eaten_y + ' '
     return orders
                         
